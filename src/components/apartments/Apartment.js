6a964639b801c3837bb77cfd1222b1bbfed92c7a
Stephen V. Nelson <StephenVNelson@gmail.com>
2020-04-14 15:47:55 -0700

    updated property calls

--- src/components/apartments/Apartment.js
+++ src/components/apartments/Apartment.js
@@ -3,10 +3,11 @@ import PropTypes from "prop-types";
 
 const Apartment = ({ apartment }) => {
   const a = apartment.fields
-  const building = a?.building?.fields;
+  const building = a.building.fields;
   const fullAddress = `${building?.address}\n${building?.city}, ${building?.state} ${building?.zipCode}`
+  const roommateGroup = a.roommateGroup.fields
+  const rent = Math.round(a.rent / ((roommateGroup?.prospects.length + 1) || 1))
   // get the rent and divide it by total number of people
-  const rent = a.Rent
   const sharing = "Private Room"
   return (
     <li>
@@ -15,11 +16,11 @@ const Apartment = ({ apartment }) => {
         <div className="apartment-info">
           <ul>
             <li>{fullAddress}</li>
-            <li>Apt. {a.Unit}</li>
-            <li>{a.Bedrooms} bedroom {a.Bedrooms} bathroom, {a.SqFt} sq/ft.</li>
+            <li>Apt. {a.unit}</li>
+            <li>{a.Bedrooms} bedroom {a.bedrooms} bathroom, {a.sqft} sq/ft.</li>
             <li>${rent}/month</li>
             <li>{sharing}</li>
-            <li>lease: {a["Lease In Months"]} months</li>
+            <li>lease: {a.leaseInMonths} months</li>
           </ul>
         </div>
       </div>
