7f6a0fe659559d54ad51da0209f6fb60ecd58d67
Stephen V. Nelson <StephenVNelson@gmail.com>
2020-06-03 15:30:56 -0700

    switched to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css"
@@ -8,7 +9,7 @@ import configureStore from './redux/configureStore';
 import { Provider as ReduxProvider } from 'react-redux';
 
 const store = configureStore();
-console.log("PUBLIC URL: ", `backtopublic/${process.env.PUBLIC_URL}`)
+console.log("PUBLIC URL: ", `hashrouter/${process.env.PUBLIC_URL}`)
 // baseName={process.env.PUBLIC_URL} not sure if I need this
 render(
   <ReduxProvider store={store}>
