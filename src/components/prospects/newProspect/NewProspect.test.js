203fb6bfeebe12962d3fbe68819cf5a3876d66f9
Stephen V. Nelson <StephenVNelson@gmail.com>
2020-06-11 22:26:27 -0700

    comment out NewProspect test for now

--- src/components/prospects/newProspect/NewProspect.test.js
+++ src/components/prospects/newProspect/NewProspect.test.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import NewProspect from './NewProspect'
-import { mount } from 'enzyme'
-import { MemoryRouter } from 'react-router-dom'
-import { newApartment, newRoommateGroup, newProspect } from '../../../../tools/mockData'
+// import React from 'react'
+// import NewProspect from './NewProspect'
+// import { mount } from 'enzyme'
+// // import { MemoryRouter } from 'react-router-dom'
+// import { apartment, roommateGroup, prospects, session } from '../../../../tools/testMock'
 
-// function renderNewProspect(args) {
-// const defaultProps = {
-//   apartment:,
-//   saveApartment,
-//   roommateGroup,
-//   saveRoommateGroup,
-//   prospects,
-//   session,
-//   createSession,
-//   updateSession,
-//   saveProspect,
-//   history
+// function render(args) {
+//   const defaultProps = {
+//     apartment,
+//     saveApartment: jest.fn(),
+//     roommateGroup,
+//     saveRoommateGroup: jest.fn(),
+//     prospects,
+//     session,
+//     createSession: jest.fn(),
+//     updateSession: jest.fn(),
+//     saveProspect: jest.fn(),
+//     history: []
+//   }
+//   const props = { ...defaultProps, ...args }
+//   return mount(<NewProspect {...props} />)
 // }
-// }
-it("should be true", () => {
+it("does something", () => {
+  // const wrapper = render()
   expect(true).toBe(true)
 })
