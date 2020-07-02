5d4aaef412d23d7657e428c1e0988c79d0321095
Stephen V. Nelson <StephenVNelson@gmail.com>
2020-07-02 13:13:13 -0700

    transfer css from ProspectInfo to FormContainer

--- src/components/apartments/apartment/application/steps/FormContainer.js
+++ src/components/apartments/apartment/application/steps/FormContainer.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import './FormContainer.css'
 
 export const FormContainer = ({ children, unit }) => {
 
   return (
     <>
-      <div className="prospect-info-container">
-        <div className="prospect-info-header">
-          <div className="prospect-info-header__title">SIGNUP FORM | UNIT {unit}</div>
+      <div className="form-container">
+        <div className="form-header">
+          <div className="form-header__title">SIGNUP FORM | UNIT {unit}</div>
           <span>step 1:</span> <span style={{ fontWeight: "100" }}>fill in your info</span>
         </div>
-        <div className="prospect-info-contents">
+        <div className="form-contents">
           {children}
         </div>
       </div>
