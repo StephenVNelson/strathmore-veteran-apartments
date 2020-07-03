b6f621c8185c16321e5336c65334f0f970cdb481
Stephen V. Nelson <StephenVNelson@gmail.com>
2020-07-03 10:21:51 -0700

    added configuration to FormContainer

--- src/components/apartments/apartment/application/steps/FormContainer.js
+++ src/components/apartments/apartment/application/steps/FormContainer.js
@@ -2,14 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import './FormContainer.css'
 
-export const FormContainer = ({ children, unit }) => {
-
+export const FormContainer = ({ children, unit, step }) => {
+  const instructions = () => {
+    switch (step) {
+      case 1:
+        return "fill in your info"
+      case 2:
+        return "Roommate setup"
+      default:
+        "fill in your info"
+    }
+  }
   return (
     <>
       <div className="form-container">
         <div className="form-header">
           <div className="form-header__title">SIGNUP FORM | UNIT {unit}</div>
-          <span>step 1:</span> <span style={{ fontWeight: "100" }}>fill in your info</span>
+          <span>step {step}:</span> <span style={{ fontWeight: "100" }}>{instructions()}</span>
         </div>
         <div className="form-contents">
           {children}
